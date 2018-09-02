68602fbb975f2e2239ec1eb2c517532d4c663052
cvmartins1996 <cvmartins1996@gmail.com>
2018-09-02 20:46:06 -0300

    primeiro commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+M.AutoInit();
+ 
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
+  });
